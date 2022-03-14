9d3b0b44e6dca4235906470a8fba128a1848f8a7
Uma Prasad Das <61022623+CodeWithUma@users.noreply.github.com>
2022-03-14 22:17:37 +0530

    Add files via upload

--- 1-my-first-app-practice-set/src/app/components/displays/displays.component.ts
+++ 1-my-first-app-practice-set/src/app/components/displays/displays.component.ts
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplaysComponent implements OnInit {
 
-  constructor() { }
+  showSecret = false;
+  log = [];
 
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+
+  constructor() { }
+ 
   ngOnInit(): void {
   }
 
