9ba5aec7a773994e0f828a5c9700932d6d08b8bb
umaprasaddas <umaprasaddas1999@gmail.com>
2022-03-14 22:12:27 +0530

    components added

--- 1-my-first-app-practice-set/src/app/app.module.ts
+++ 1-my-first-app-practice-set/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplaysComponent } from './components/displays/displays.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
+import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
+import { UsernameComponent } from './components/username/username.component';
+import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplaysComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    UsernameComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
