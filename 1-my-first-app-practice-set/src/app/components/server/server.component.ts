e68a173645301a9beb94b178ca654b70d717e661
Uma Prasad Das <61022623+CodeWithUma@users.noreply.github.com>
2022-03-14 22:18:17 +0530

    Add files via upload

--- 1-my-first-app-practice-set/src/app/components/server/server.component.ts
+++ 1-my-first-app-practice-set/src/app/components/server/server.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
+  styles:[`
+  .online{
+    color: white;
+  }`]
 })
 export class ServerComponent implements OnInit {
+  serverID: number = 10;            // STRING INTERPOLATION METHOD HAS BEEN USED HERE
+  serverStatus: string = 'offline'; // STRING INTERPOLATION METHOD HAS BEEN USED HERE
 
-  constructor() { }
+  // HERE METHOD HAS BEEN DEFINED
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  constructor() { 
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+   }
 
   ngOnInit(): void {
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
