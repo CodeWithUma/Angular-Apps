d718415138d6403325fd24ddaae97288ebb05bbb
Uma Prasad Das <61022623+CodeWithUma@users.noreply.github.com>
2022-03-14 22:20:37 +0530

    Add files via upload

--- 1-my-first-app-practice-set/src/app/components/username/username.component.ts
+++ 1-my-first-app-practice-set/src/app/components/username/username.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./username.component.css']
 })
 export class UsernameComponent implements OnInit {
+// NEW PROPERTY BINDING HAS EEN DECLARED HERE
+username = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // onCreateServer(){
+  //   this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  // }
+
+  // onUpdateServerName(event : Event){
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
+  
 }
