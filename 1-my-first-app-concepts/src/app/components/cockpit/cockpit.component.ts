7225ac94ba8ca259909a010a3f87980f5aa6937c
umaprasaddas <umaprasaddas1999@gmail.com>
2022-03-17 12:04:11 +0530

    error solved and code updated

--- 1-my-first-app-concepts/src/app/components/cockpit/cockpit.component.ts
+++ 1-my-first-app-concepts/src/app/components/cockpit/cockpit.component.ts
@@ -7,11 +7,11 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{
-     serverName: String;
+    serverName: string;
     serverContent: string;
   }>();
-  @Output() blueprintCreated = new EventEmitter<{
-    serverName: String;
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
+    serverName: string;
     serverContent: string;
   }>();
   newServerName = '';
