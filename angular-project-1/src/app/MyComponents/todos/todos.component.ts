0389bac25093a6bbcb9dbb94053baa95920fd70b
umaprasaddas <umaprasaddas1999@gmail.com>
2022-03-31 23:18:54 +0530

    ngfor added

--- angular-project-1/src/app/MyComponents/todos/todos.component.ts
+++ angular-project-1/src/app/MyComponents/todos/todos.component.ts
@@ -12,14 +12,33 @@ export class TodosComponent implements OnInit {
 
   constructor() { 
     this.todos = [
-      sno: 3,
-      title: "This is title",
+    {
+      sno: 1,
+      title: "This is title 1",
       desc: "Description",
       active: true
-    ]
+    },
+    {
+      sno: 2,
+      title: "This is title 2",
+      desc: "Description",
+      active: true
+    },
+    {
+      sno: 3,
+      title: "This is title 3",
+      desc: "Description",
+      active: true
+    }
+  ]
   }
 
   ngOnInit(): void {
   }
 
+  deleteTodo(todo: Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
 }
