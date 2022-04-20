3f898d9c0957c0ee960b69b6ab1de3f959033961
umaprasaddas <umaprasaddas1999@gmail.com>
2022-04-20 20:58:19 +0530

    login component added

---
+++ login-form/src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  onSubmit(loginForm: NgForm) {
+    console.log(loginForm);
+    // const { email, password } = loginForm.value;
+    // this.authService.onLogin(email, password).subscribe((response: any) => {
+    //   if (response.message === 'SUCCESS') {
+    //     this.router.navigate(['/users']);
+    //   }
+    // });
+  }
+
+  onReset(loginForm: NgForm) {
+    loginForm.reset();
+  }
+}
