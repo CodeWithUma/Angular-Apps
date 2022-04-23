4d250253b60f090f5d2b07c3e1e9de83639ae35f
umaprasaddas <umaprasaddas1999@gmail.com>
2022-04-23 23:13:03 +0530

    outsourcing the route configuration

--- 1-my-first-app-routing/src/app/app-routing.module.ts
+++ 1-my-first-app-routing/src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { EditServerComponent } from './components/servers/edit-server/edit-server.component';
+import { ServerComponent } from './components/servers/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
+import { UserComponent } from './components/users/user/user.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [{ path: ':id/:name', component: UserComponent }],
+  },
+
+  {
+    path: 'servers',
+    component: ServersComponent,
+    children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ],
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found', pathMatch: 'full' }, // ** means any other route can be redirect to not-found route
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- 1-my-first-app-routing/src/app/app.module.ts
+++ 1-my-first-app-routing/src/app/app.module.ts
@@ -11,28 +11,8 @@ import { ServerComponent } from './components/servers/server/server.component';
 import { EditServerComponent } from './components/servers/edit-server/edit-server.component';
 import { FormsModule } from '@angular/forms';
 import { ServersService } from './components/servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [{ path: ':id/:name', component: UserComponent }],
-  },
-
-  {
-    path: 'servers',
-    component: ServersComponent,
-    children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
-    ],
-  },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/not-found', pathMatch: 'full' }, // ** means any other route can be redirect to not-found route
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,12 +24,7 @@ const appRoutes: Routes = [
     EditServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
