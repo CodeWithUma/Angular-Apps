835e613a4f81d19cef491aa5e44bdd0b0945865e
umaprasaddas <umaprasaddas1999@gmail.com>
2022-04-26 15:22:05 +0530

    analyzing angular observables

--- 1-my-first-app-observables/src/app/app.module.ts
+++ 1-my-first-app-observables/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
