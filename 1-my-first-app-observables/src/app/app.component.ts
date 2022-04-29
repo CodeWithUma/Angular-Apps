aa5a65458e2272f8327efa9d5d279883f3793bca
umaprasaddas <umaprasaddas1999@gmail.com>
2022-04-29 22:38:56 +0530

    Subjects

--- 1-my-first-app-observables/src/app/app.component.ts
+++ 1-my-first-app-observables/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = '1-my-first-app-observables';
+  private activatedSub: Subscription | any;
+
+  userActivated = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
+      (didActivate) => {
+        this.userActivated = didActivate;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
 }
