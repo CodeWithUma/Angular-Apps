45ed9ee625d163657a0fa6a630a34354839d9a60
umaprasaddas <umaprasaddas1999@gmail.com>
2022-04-30 17:40:34 +0530

    Submitting and Using the form

--- 1-my-first-app-handling-forms/src/app/app.component.ts
+++ 1-my-first-app-handling-forms/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '1-my-first-app-handling-forms';
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.user.setFirstName(suggestedName);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
