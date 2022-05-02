19873d963a1c0dbd597a549b3b7c8eacbee6ad45
umaprasaddas <umaprasaddas1999@gmail.com>
2022-05-02 21:19:22 +0530

    Reactive: Setup

--- 1-my-first-app-reactive-handling-forms/src/app/app.component.ts
+++ 1-my-first-app-reactive-handling-forms/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '1-my-first-app-reactive-handling-forms';
+  genders = ['male', 'female'];
+  signupForm: FormGroup | any;
 }
