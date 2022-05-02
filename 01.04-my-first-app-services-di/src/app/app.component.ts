ad96855a763422cfc34c6309fc7f563b08f683b4
umaprasaddas <umaprasaddas1999@gmail.com>
2022-05-02 21:52:07 +0530

    code updated

---
+++ 01.04-my-first-app-services-di/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AccountService } from './account.services';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = '01.04-my-first-app-services-di';
+  accounts: { name: string; status: string }[] = [];
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit() {
+    this.accounts = this.accountService.accounts;
+  }
+}
---
+++ 01.05-my-first-app-asgmt-5/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UserService } from './users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [UserService],
+})
+export class AppComponent {
+  title = '01.05-my-first-app-asgmt-5';
+}
