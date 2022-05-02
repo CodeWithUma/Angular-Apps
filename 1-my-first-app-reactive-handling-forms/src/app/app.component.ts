759cf68d5cbd49cbac6f585c35f034719486e720
umaprasaddas <umaprasaddas1999@gmail.com>
2022-05-02 21:24:24 +0530

    Reactive: Creating a Form in Code

--- 1-my-first-app-reactive-handling-forms/src/app/app.component.ts
+++ 1-my-first-app-reactive-handling-forms/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '1-my-first-app-reactive-handling-forms';
   genders = ['male', 'female'];
   signupForm: FormGroup | any;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl('male'),
+    });
+  }
 }
