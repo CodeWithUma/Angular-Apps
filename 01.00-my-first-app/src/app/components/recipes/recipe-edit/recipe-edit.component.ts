dfc3cb506e5118f7cff5c5e37635ef21ac4f4707
umaprasaddas <umaprasaddas1999@gmail.com>
2022-05-04 23:18:40 +0530

    Validating User Input

--- 01.00-my-first-app/src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ 01.00-my-first-app/src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -33,8 +33,11 @@ export class RecipeEditComponent implements OnInit {
   onAddIngredient() {
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
-        name: new FormControl(),
-        amount: new FormControl(),
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
       })
     );
   }
@@ -66,9 +69,9 @@ export class RecipeEditComponent implements OnInit {
     }
 
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription),
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
       ingredients: recipeIngredients,
     });
   }
