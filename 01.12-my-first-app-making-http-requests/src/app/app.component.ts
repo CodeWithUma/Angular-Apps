114ec6771530a4b655581c4647ff2d1a2dc53384
Uma Das <umaprasaddas1999@gmail.com>
2022-07-03 12:25:30 +0530

    bootstrap elements added

--- 01.12-my-first-app-making-http-requests/src/app/app.component.ts
+++ 01.12-my-first-app-making-http-requests/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '01.12-my-first-app-making-http-requests';
+
+  loadedPosts = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
 }
